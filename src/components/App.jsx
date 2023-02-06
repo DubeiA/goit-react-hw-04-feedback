de045151eac94069f300e55b6c2f536649b1f3a2
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-02-06 14:31:55 +0200

    Done task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,81 @@
-export const App = () => {
+import {useState} from 'react'
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics'
+import { FeedbackOptions } from "./Feedback/FeedbackOptions";
+import {Notification} from "./Notification/Notification"
+
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedback = {good, neutral, bad}
+
+  const handleIncrementReview = (e) => {
+   
+    const {name} = e.target
+    
+    switch (name) { 
+      case 'good':
+        setGood(prevGood => prevGood + 1)
+        break;
+      
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1)
+        break;
+      
+      case 'bad':
+        setBad(prevBad => prevBad + 1)
+        break;
+      
+      default:
+        console.log('We dont understand what you have done');
+    }
+  };
+
+  const countTotalFeedback = () => { 
+  return good + neutral + bad
+    
+  }
+
+  const handltPositive = () => {
+    console.log();
+    return (good * 100 / (good + neutral + bad)).toFixed(2)
+  }
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
+   <div
+       style={{
+        textAlign: 'center',
+        fontSize: 24,
         color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+      }}>
+       
+
+        <Section title = "Please leave your feedback">
+           <FeedbackOptions options={Object.keys(feedback)} onLeaveFeedback={handleIncrementReview}></FeedbackOptions>
+       </Section>
+     
+
+         
+        <Section title='Statistick'>
+          {countTotalFeedback() > 0
+          ? <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={handltPositive()}>
+          </Statistics>
+          : <Notification message='There is no feedback' />}
+           </Section>
+         
+        </div>
+  )
+}
+
+
+       
+     
+   
+
